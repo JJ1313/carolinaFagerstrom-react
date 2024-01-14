d5b03795d129354a51bdaf549099e721961f4416
JJ1312 <yeiyeithirteen@gmail.com>
2024-01-13 21:36:03 -0300

    Agregan mas clases utiles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export function Hero() {
     <>
       <section id="hero">
         <div className="hero-content">
-          <h2 className="hero-title">Carolina Fagerström</h2>
+          <h1 className="hero-title">Carolina Fagerström</h1>
           <p className="hero-text">Pianista y compositora apasionada por la música clásica y contemporánea</p>
         </div>
       </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,15 @@ import "../assets/css/navbar.css"
 export function Navbar() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Work</li>
-          <li>News</li>
+      <nav id="navbar" className="d-flex bg-black">
+        <div className="h1 ff-hand">Carolina Fagerstrom</div>
+        <ul className="d-flex g-2">
+          <li><a className="link" href="/">Inicio</a></li>
+          <li><a className="link" href="/">Sobre Mi</a></li>
+          <li><a className="link" href="/news">Noticias</a></li>
+          <li className="link">Obras</li>
+          <li><a className="link" href="/news">Contacto</a></li>
+          <li className="link">EN</li>
         </ul>
       </nav>
     </>
