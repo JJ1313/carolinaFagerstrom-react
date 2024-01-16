4eeb90879e916717ff9cd3ed3c19b3f096500cab
JJ1312 <yeiyeithirteen@gmail.com>
2024-01-16 16:48:51 -0300

    Avanza en navbar y en hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import "../assets/css/hero.css"
 export function Hero() {
   return (
     <>
-      <section id="hero">
-        <div className="hero-content">
-          <h1 className="hero-title">Carolina Fagerström</h1>
+      <section id="hero" className="d-grid align-items-center justify-content-center">
+        <div className="hero-content txt-center txt-white">
+          <h1 className="hero-title mb-2">Carolina Fagerström</h1>
           <p className="hero-text">Pianista y compositora apasionada por la música clásica y contemporánea</p>
         </div>
       </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,19 @@ import "../assets/css/navbar.css"
 export function Navbar() {
   return (
     <>
-      <nav id="navbar" className="d-flex bg-black">
-        <div className="h1 ff-hand">Carolina Fagerstrom</div>
-        <ul className="d-flex g-2">
-          <li><a className="link" href="/">Inicio</a></li>
-          <li><a className="link" href="/">Sobre Mi</a></li>
-          <li><a className="link" href="/news">Noticias</a></li>
-          <li className="link">Obras</li>
-          <li><a className="link" href="/news">Contacto</a></li>
-          <li className="link">EN</li>
+      <nav id="navbar" className="d-flex bg-black-0 align-items-center justify-content-between px-4">
+        <div className="h1 ff-hand txt-white link"><a href="">Carolina Fagerström</a></div>
+        <ul className="d-flex g-15">
+          <li><a className="link txt-white txt-upper" href="/">Inicio</a></li>
+          <li><a className="link txt-white txt-upper" href="/">Sobre Mi</a></li>
+          <li><a className="link txt-white txt-upper" href="/news">Noticias</a></li>
+          <li id="dropdown-obras" className="link txt-white txt-upper dropdown">Obras</li>
+          <li><a className="link txt-white txt-upper" href="/news">Contacto</a></li>
+          <li className="link txt-white txt-upper">EN</li>
+        </ul>
+        <ul id="menu-obras" className="dropdown-menu" anchor="#dropdown-obras">
+          <li>Conciertos</li>
+          <li>Multimedia</li>
         </ul>
       </nav>
     </>
