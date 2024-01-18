cc73e1a27028ecd96220815d3a7819ef88464ed3
jj <hgbeckerss@gmail.com>
2024-01-18 17:04:49 -0300

    Reemplaza icono de menu dropdown y agrega componente vacio About

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,15 @@ export function Navbar() {
         <div className="h1 ff-hand txt-white link d-block my-auto"><a className="" href="/">Carolina Fagerström</a></div>
         <ul className="d-flex g-15 ms-auto pt-1">
           <li className=""><a className="link txt-white txt-upper fw-600 fs-08 ff-sec d-block" href="/">Inicio</a></li>
-          <li><a className="link txt-white txt-upper fw-600 fs-08 d-block" href="/">Sobre Mi</a></li>
+          <li><a className="link txt-white txt-upper fw-600 fs-08 d-block" href="/#about">Sobre Mi</a></li>
           <li><a className="link txt-white txt-upper fw-600 fs-08 d-block" href="/news">Noticias</a></li>
-          <li id="dropdown-obras" className="link txt-white txt-upper dropdown fw-600 fs-08 pb-1"><a href="/news">Obras</a>
+          <li id="dropdown-obras" className="link txt-white txt-upper dropdown fw-600 fs-08 pb-1"><a href="/work">Obras <span className="dropdown-icon"></span></a>
             <ul id="menu-obras" className="dropdown-menu bg-white mt-1">
-              <li className="txt-black link txt-cap fw-600 fs-08 mb-1">Conciertos</li>
-              <li className="txt-black link txt-cap fw-600 fs-08">Multimedia</li>
+              <li className="txt-black link txt-cap fw-600 fs-08 mb-1"><a href="/work?concerts">Conciertos</a></li>
+              <li className="txt-black link txt-cap fw-600 fs-08"><a href="/work?multimedia">Multimedia</a></li>
             </ul>
           </li>
-          <li><a className="link txt-white txt-upper fw-600 fs-08 d-block" href="/news">Contacto</a></li>
+          <li><a className="link txt-white txt-upper fw-600 fs-08 d-block" href="#contact">Contacto</a></li>
         </ul >
         <button className="link txt-white txt-upper fw-600 fs-08 ms-15 my-auto">EN</button>
       </nav >
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Contact } from "../components/Contact"
 import { Hero } from "../components/Hero"
+import { About } from "../components/About"
 export function Home() {
 
   return (
     <>
       <Hero></Hero>
+      <About></About>
       <Contact></Contact>
     </>
   )
