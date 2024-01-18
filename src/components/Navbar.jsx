3d40b6c9a0ed3adbadd5bffac3c2b486953132cb
jj <hgbeckerss@gmail.com>
2024-01-18 12:59:06 -0300

    Apariencia final de navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,21 @@ import "../assets/css/navbar.css"
 export function Navbar() {
   return (
     <>
-      <nav id="navbar" className="d-flex bg-black-0 align-items-center justify-content-between px-4">
+      <nav id="navbar" className="d-flex bg-black-0 align-items-center justify-content-start px-4">
         <div className="h1 ff-hand txt-white link"><a href="">Carolina Fagerström</a></div>
-        <ul className="d-flex g-15">
-          <li><a className="link txt-white txt-upper" href="/">Inicio</a></li>
-          <li><a className="link txt-white txt-upper" href="/">Sobre Mi</a></li>
-          <li><a className="link txt-white txt-upper" href="/news">Noticias</a></li>
-          <li id="dropdown-obras" className="link txt-white txt-upper dropdown">Obras
-            <ul id="menu-obras" className="dropdown-menu bg-white p-1" anchor="#dropdown-obras">
-              <li className="txt-black link">Conciertos</li>
-              <li className="txt-black link">Multimedia</li>
+        <ul className="d-flex g-15 ms-auto">
+          <li><a className="link txt-white txt-upper fw-600 fs-08 ff-sec" href="/">Inicio</a></li>
+          <li><a className="link txt-white txt-upper fw-600 fs-08" href="/">Sobre Mi</a></li>
+          <li><a className="link txt-white txt-upper fw-600 fs-08" href="/news">Noticias</a></li>
+          <li id="dropdown-obras" className="link txt-white txt-upper dropdown fw-600 fs-08">Obras
+            <ul id="menu-obras" className="dropdown-menu bg-white p-1 mt-1">
+              <li className="txt-black link txt-cap fw-600 fs-08 mb-1">Conciertos</li>
+              <li className="txt-black link txt-cap fw-600 fs-08">Multimedia</li>
             </ul>
           </li>
-          <li><a className="link txt-white txt-upper" href="/news">Contacto</a></li>
-          <li className="link txt-white txt-upper">EN</li>
+          <li><a className="link txt-white txt-upper fw-600 fs-08" href="/news">Contacto</a></li>
         </ul>
+        <a className="link txt-white txt-upper fw-600 fs-08 ms-15">EN</a>
       </nav>
     </>
   )
