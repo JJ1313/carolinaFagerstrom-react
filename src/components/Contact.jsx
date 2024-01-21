d9d497a5ec8412b330b08b9a4ff6c4d2bac93d3c
jj <hgbeckerss@gmail.com>
2024-01-21 13:53:02 -0300

    Termina seccion noticias de Home

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,28 @@
+import "../assets/css/contact.css"
 export function Contact() {
   return (
-    <>
-      <form>
+    <div id="contact" className="d-grid cols cols-md-2 mt-10 px-4">
+      <div className="d-flex flex-column">
+        <h3>Contacto</h3>
+        <a href="" className="txt-gray-5"><span>Ic</span> Con Con, Chile</a>
+        <a href="" className="txt-gray-5"><span>Ic</span> +56 9 7436 7083</a>
+        <a href="" className="txt-gray-5"><span>Ic</span> lanierpal@gmail.com</a>
+      </div>
+      <form className="d-grid">
         <div className="input-container">
-          <label htmlFor="name">Nombre</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" aria-label="Nombre" placeholder="Nombre" className="txt-upper fs-08 fw-500" />
         </div>
         <div className="input-container">
-          <label htmlFor="email">Correo</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" aria-label="Correo" placeholder="Correo" className="txt-upper " />
         </div>
         <div className="input-container">
-          <label htmlFor="message">Mensaje</label>
-          <textarea id="message" name="email" rows="10" cols="50" />
+          <input type="text" id="subject" aria-label="Asunto" placeholder="Asunto" className="txt-upper " />
         </div>
-        <button type="submit">enviar</button>
+        <div className="input-container">
+          <textarea id="message" name="email" aria-label="Mensaje" placeholder="Mensaje" className="txt-upper " />
+        </div>
+        <button type="submit" className="btn txt-upper">enviar mensaje</button>
       </form>
-    </>
+    </div>
   )
 }
--- src/components/NewsHome.jsx
+++ src/components/NewsHome.jsx
@@ -4,14 +4,14 @@ import "../assets/css/news.css"
 export function NewsHome() {
 
   return (
-    <div className="mt-10 d-grid justify-content-center align-items-center">
+    <div id="news" className="mt-10 d-grid justify-content-center align-items-center">
       <h2 className="txt-center mb-3 fw-600 fs-16 txt-upper ff-secondary">Noticias</h2>
       <div className="d-grid cols cols-md-2 cols-lg-3 px-4 g-3">
         <Card title="Titulo 1" img="src\assets\img\piano.jpg" link=""></Card>
         <Card title="Titulo 2" img="src\assets\img\piano.jpg" link=""></Card>
         <Card title="Titulo 3" img="src\assets\img\piano.jpg" link=""></Card>
       </div>
-      <a href="/news" className="btn">Ver más</a>
+      <a href="/news" className="btn mx-auto mt-2">Ver más</a>
     </div>
   )
 }
