c04bb5eff88599d19a8cc3ec006be9ecfb80a169
jj <hgbeckerss@gmail.com>
2024-01-22 15:46:31 -0300

    font size y font weight de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,28 @@
+import "../assets/css/footer.css"
+
 export function Footer() {
   return (
     <>
-      <footer className="d-flex bg-black-0 pt-3p75 txt-white mt-5">
-        <h2>Carolina Fagerström</h2>
+      <footer id="footer" className="d-flex bg-black-0 pt-3p75 txt-white mt-5">
+        <h2 className="h1 ff-hand"><a href="/" className="link">Carolina Fagerström</a></h2>
         <div>
-          <h3 className="txt-upper">Contacto</h3>
-          <ul>
-            <li>Home</li>
-            <li>News</li>
-            <li>Work</li>
+          <h3 className="txt-upper fs-0p8 fw-700">Contacto</h3>
+          <ul className="navbar">
+            <li><a href="/" className="link fw-300">Inicio</a></li>
+            <li><a href="/news" className="link fw-300">Noticias</a></li>
+            <li><a href="/work" className="link fw-300">Obras</a></li>
           </ul>
         </div>
         <div>
-          <h3 className="txt-upper">Contáctame</h3>
+          <h3 className="txt-upper fs-0p8 fw-700">Contáctame</h3>
           <ul>
-            <li><a href="tel:">Telefono</a></li>
-            <li><a href="mailto:">correo</a></li>
+            <li><a href="tel:+56974367083" className="link fw-300">+56 9 7436 7083</a></li>
+            <li><a href="mailto:lanierpal@gmail.com" className="link fw-300">lanierpal@gmail.com</a></li>
           </ul>
           <ul>
             <li><a href="http://">YT</a></li>
           </ul>
         </div>
-
       </footer>
     </>
   )
