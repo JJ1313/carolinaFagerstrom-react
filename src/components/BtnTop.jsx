81d4f5afca8172c24e88675a68d57b32a4768bb9
jj <hgbeckerss@gmail.com>
2024-01-22 14:23:01 -0300

    Agrega funcionalidad a btn to top

--- src/components/BtnTop.jsx
+++ src/components/BtnTop.jsx
@@ -1,7 +1,10 @@
 import "../assets/css/btnTop.css"
 export function BtnTop() {
+  function goTop() {
+    window.scrollTo(0, 0);
+  }
   return (
-    <button id="btn-top">
+    <button id="btn-top" onClick={goTop}>
     </button>
   )
 }
