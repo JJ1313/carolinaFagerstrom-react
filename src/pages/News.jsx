0ede2cd8054851617e991c5a25469cd1bd4e75da
jj <hgbeckerss@gmail.com>
2024-01-23 16:25:57 -0300

    Esqueleto de pagina News

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,8 +1,24 @@
+
+import { Card } from "../components/Card.jsx"
+import "../assets/css/news.css"
 export function News() {
 
   return (
-    <>
-      <h1>1312!!! News.jsx</h1>
-    </>
+    <section className="section mt-10">
+      <h2 className="mb-3 fw-600 fs-1p6 txt-upper ff-secondary txt-center">Noticias</h2>
+      <div className="d-grid cols cols-md-2 cols-lg-3 g-1">
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+      </div>
+    </section>
   )
 }
