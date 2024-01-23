517b9d6a3a73b42e8c4927913e5d7c22ede86c25
jj <hgbeckerss@gmail.com>
2024-01-22 21:53:40 -0300

    limpieza de codigo - eliminan tags extras

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,11 @@
 import "../assets/css/hero.css"
 export function Hero() {
   return (
-    <>
-      <section id="hero" className="d-grid align-items-center justify-content-center">
-        <div className="hero-content txt-center txt-white">
-          <h1 className="hero-title mb-2 px-1 fs-3 fw-700">Carolina Fagerström</h1>
-          <p className="hero-text px-1 fw-200">Pianista y compositora apasionada por la música clásica y contemporánea</p>
-        </div>
-      </section>
-    </>
+    <section id="hero" className="d-grid align-items-center justify-content-center">
+      <div className="hero-content txt-center txt-white">
+        <h1 className="hero-title mb-2 px-1 fs-3 fw-700">Carolina Fagerström</h1>
+        <p className="hero-text px-1 fw-200">Pianista y compositora apasionada por la música clásica y contemporánea</p>
+      </div>
+    </section>
   )
 }
