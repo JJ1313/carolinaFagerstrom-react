9be7c9f6f4014b6828947d64cd2cffe35626b73d
jj <hgbeckerss@gmail.com>
2024-01-26 15:51:20 -0300

    agrega render condicional para cuando la noticia no tiene link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,27 @@ import "../assets/css/card.css"
 import { useState } from "react";
 export function Card(props) {
   const [imgIsLoaded, setImgIsLoaded] = useState(false)
+  const wrapperStyle = {
+    backgroundImage: `url(${props.imgSmall})`,
+  }
+  console.log(props.link)
 
   return (
     < article className="card hover-shadow" >
-      <a href={props.link} >
-        <div className="blur-load" style={{ backgroundImage: `url(${props.imgSmall})` }}>
-          < img style={imgIsLoaded ? { opacity: 1 } : { opacity: 0 }} src={props.img} onLoad={() => { setImgIsLoaded(true) }} loading="lazy" alt="" />
-        </div>
-        <h3 className="txt-alert fw-600 fs-1p2 p-1">{props.title}</h3>
-      </a >
-    </ article>
+      {props.link ?
+        <a href={props.link} target="_blank" className="active">
+          <div className="img-wrapper blur-load" style={wrapperStyle} data-loaded={imgIsLoaded}>
+            < img style={imgIsLoaded ? { opacity: 1 } : { opacity: 0 }} src={props.img} onLoad={() => { setImgIsLoaded(true) }} loading="lazy" />
+          </div>
+          <h3 className="txt-alert fw-600 fs-1p2 p-1">{props.title}</h3>
+        </a > :
+        <a target="_blank">
+          <div className="img-wrapper blur-load" style={wrapperStyle} data-loaded={imgIsLoaded}>
+            < img style={imgIsLoaded ? { opacity: 1 } : { opacity: 0 }} src={props.img} onLoad={() => { setImgIsLoaded(true) }} loading="lazy" />
+          </div>
+          <h3 className="txt-alert fw-600 fs-1p2 p-1">{props.title}</h3>
+        </a >
+      }
+    </ article >
   )
 }
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -8,8 +8,8 @@ export function News() {
     <section className="section mt-10">
       <h2 className="mb-3 fw-600 fs-1p6 txt-upper ff-secondary txt-center">Noticias</h2>
       <div className="d-grid cols cols-md-2 cols-lg-3 g-1">
-        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Conciertos de la tarde en el Parque: obras chilenas y Brahms" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="www.google.com" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
         <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
         <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
         <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
