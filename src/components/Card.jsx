50b854e722382e7aa9fe40034a5cf0d998bacec6
jj <hgbeckerss@gmail.com>
2024-01-26 11:00:45 -0300

    Agrega lazy loading a ima de componente card, cargando una en baja resoucion primeo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
 import "../assets/css/card.css"
+import { useState } from "react";
 export function Card(props) {
+  const [imgIsLoaded, setImgIsLoaded] = useState(false)
+
   return (
-    <div className="card hover-shadow">
+    < div className="card hover-shadow" >
       <a href={props.link} >
-        <img src={props.img} loading="lazy" alt="" />
+        <div className="blur-load" style={{ backgroundImage: `url(${props.imgSmall})` }}>
+          < img style={imgIsLoaded ? { opacity: 1 } : { opacity: 0 }} src={props.img} onLoad={() => { setImgIsLoaded(true) }} loading="lazy" alt="" />
+        </div>
         <h3 className="txt-alert fw-600 fs-1p2 p-1">{props.title}</h3>
-      </a>
-    </div>
+      </a >
+    </ div>
   )
 }
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,21 +3,21 @@ import { Card } from "../components/Card.jsx"
 import "../assets/css/news.css"
 export function News() {
 
+
   return (
     <section className="section mt-10">
       <h2 className="mb-3 fw-600 fs-1p6 txt-upper ff-secondary txt-center">Noticias</h2>
       <div className="d-grid cols cols-md-2 cols-lg-3 g-1">
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
-        <Card title="Titulo 1" link="" img="src\assets\img\piano.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
+        <Card title="Titulo 1" link="" img="src/assets/img/piano.jpg" imgSmall="src/assets/img/piano-small.jpg"></Card>
       </div>
     </section>
   )
