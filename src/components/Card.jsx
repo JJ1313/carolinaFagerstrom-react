11c873de0cea8fcba98a431153178270c41f4516
jj <hgbeckerss@gmail.com>
2024-01-26 11:31:38 -0300

    div -> article de card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,13 +4,13 @@ export function Card(props) {
   const [imgIsLoaded, setImgIsLoaded] = useState(false)
 
   return (
-    < div className="card hover-shadow" >
+    < article className="card hover-shadow" >
       <a href={props.link} >
         <div className="blur-load" style={{ backgroundImage: `url(${props.imgSmall})` }}>
           < img style={imgIsLoaded ? { opacity: 1 } : { opacity: 0 }} src={props.img} onLoad={() => { setImgIsLoaded(true) }} loading="lazy" alt="" />
         </div>
         <h3 className="txt-alert fw-600 fs-1p2 p-1">{props.title}</h3>
       </a >
-    </ div>
+    </ article>
   )
 }
